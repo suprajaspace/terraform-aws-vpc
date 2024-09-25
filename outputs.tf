1bb46750bbec7f496a8c0adec257fb3fefacf0ab
supraja <supra556699@gmail.com>
2024-09-26 02:43:07 +0800

    aws-vpc

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "az_info" {
+    value = data.aws_availability_zones.available
+}
+
+output "default_vpc_info" {
+    value = data.aws_vpc.default
+}
+
+output "main_route_table_info" {
+    value = data.aws_route_table.main
+}
